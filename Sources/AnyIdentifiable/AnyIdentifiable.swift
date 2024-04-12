55b3222247bf543e4f09d6654cf0a59f579cf00f
m1k3 <mshershnev.dev@gmail.com>
2024-04-12 17:41:36 +0100

    renamed

--- Sources/AnyIdentifiable/AnyIdentifiable.swift
+++ Sources/AnyIdentifiable/AnyIdentifiable.swift
@@ -1,10 +1,10 @@
-public struct AnyEnumerated<T>: Identifiable {
+public struct AnyIdentifiable<T>: Identifiable {
     public let id: Int
     public let value: T
 }
 
 extension Array {
-    public var identifiable: [AnyEnumerated<Element>] {
+    public var identifiable: [AnyIdentifiable<Element>] {
         enumerated().map { .init(id: $0, value: $1) }
     }
 }
--- Tests/AnyEnumeratedTests/AnyEnumeratedTests.swift
+++
@@ -1,5 +0,0 @@
-import XCTest
-@testable import AnyEnumerated
-
-final class AnyEnumeratedTests: XCTestCase {
-}
