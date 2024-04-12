756256e39745ac1119f477694ac2aa50664f3f8c
m1k3 <mshershnev.dev@gmail.com>
2024-04-12 16:37:47 +0100

    readme

--- Sources/SwiftUI-Identifiable/SwiftUI_Identifiable.swift
+++ Sources/SwiftUI-Identifiable/SwiftUI_Identifiable.swift
@@ -1,14 +1,10 @@
-//public struct SwiftUI_Identifiable {}
-
 extension Array {
-    struct IdentifiableArray<T>: Identifiable {
+    struct EnumeratedArray<T>: Identifiable {
         let id: Int
         let value: T
     }
 
-    var identifiable: [IdentifiableArray<Element>] {
-        enumerated().map {
-            .init(id: $0, value: $1)
-        }
+    var identifiable: [EnumeratedArray<Element>] {
+        enumerated().map { .init(id: $0, value: $1) }
     }
 }
