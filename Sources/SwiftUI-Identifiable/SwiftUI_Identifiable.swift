39da0034b953f470adcec2f588710d5d0b1c31da
m1k3 <mshershnev.dev@gmail.com>
2024-04-12 17:24:25 +0100

    public

--- Sources/SwiftUI-Identifiable/SwiftUI_Identifiable.swift
+++ Sources/SwiftUI-Identifiable/SwiftUI_Identifiable.swift
@@ -1,10 +1,10 @@
 extension Array {
-    struct EnumeratedArray<T>: Identifiable {
+    public struct EnumeratedArray<T>: Identifiable {
         public let id: Int
         public let value: T
     }
 
-    var identifiable: [EnumeratedArray<Element>] {
+    public var identifiable: [EnumeratedArray<Element>] {
         enumerated().map { .init(id: $0, value: $1) }
     }
 }
